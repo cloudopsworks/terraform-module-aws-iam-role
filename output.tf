838c481e3a37421d56a259b30312a81682172eef
Cristian Beraha <cristian@cloudops.works>
2024-07-30 20:07:28 -0300

    chore: IAM Policy outputs

---
+++ output.tf
@@ -0,0 +1,13 @@
+##
+# (c) 2024 - Cloud Ops Works LLC - https://cloudops.works/
+#            On GitHub: https://github.com/cloudopsworks
+#            Distributed Under Apache v2.0 License
+#
+
+output "iam_role_name" {
+  value = aws_iam_role.this.name
+}
+
+output "iam_role_arn" {
+  value = aws_iam_role.this.arn
+}
